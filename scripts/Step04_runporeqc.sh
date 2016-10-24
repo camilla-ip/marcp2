665521ef7952525b7a32510776bb5fecf7bbca14
camilla-ip <camilla.ip@well.ox.ac.uk>
2016-10-24 04:43:10 +0100

    dir fixes

--- scripts/Step04_runporeqc.sh
+++ scripts/Step04_runporeqc.sh
@@ -1,32 +1,24 @@
 #!/usr/bin/env bash
 
-
 # VARIABLES
 MP2_DIR=`cat config.txt | grep MP2_DIR | cut -f2 -d'='`
 MP2_BIN=`cat config.txt | grep MP2_BIN | cut -f2 -d'='`
 MP2_DAT=`cat config.txt | grep MP2_DAT | cut -f2 -d'='`
 
-## Create a single status.txt file
-#if [ ! -f $MP2_DAT/04-poreqc/status.txt ] ; then
-#    echo "Finished" > $MP2_DAT/04-poreqc/status.txt
-#fi
-
 # Create one shell script of poreqc commands for each experiment
-
 tail -n +2 expt.txt | while read exptid phase lab replicate libtype dirpath ; do
-
     # Create outdir
     outdir=${MP2_DAT}/04-poreqc/${exptid}
     if [ ! -d ${outdir} ] ; then
         mkdir -p ${outdir}
-
+    fi
     # Create shell script containing poreqc commands
     shfile=${outdir}/${exptid}_poreqc.sh
     if [ -f ${shfile} ] ; then
         rm ${shfile}
         touch ${shfile}
     fi
-    echo "#!/usr/bin/env bash" >> ${shfile}
+    echo "\#!/usr/bin/env bash" >> ${shfile}
     echo >> ${shfile}
     echo "echo \"Started\"" >> ${shfile}
     echo >> ${shfile}
@@ -55,9 +47,6 @@ tail -n +2 expt.txt | while read exptid phase lab replicate libtype dirpath ; do
     echo >> ${shfile}
     echo "echo \"Finished\"" >> ${shfile}
     chmod a+x ${shfile}
-
-    # Execute the shell script with nohup
-    nohup nice ${shfile} &> ${shfile}.nohup.out &
-
+    # Print the commands to be executed
+    echo "nohup nice ${shfile} &> ${shfile}.nohup.out &"
 done
-
